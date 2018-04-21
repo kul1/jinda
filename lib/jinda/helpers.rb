5e970c3697322be7d2258132f500b0b73cf8d382
P Kul <1.0@kul.asia>
2018-04-20 23:21:01 -0500

    test jinda.mm

--- lib/jinda/helpers.rb
+++ lib/jinda/helpers.rb
@@ -11,6 +11,7 @@
 module Jinda
   module Helpers
     require "rexml/document"
+		require 'pry'
     include REXML
     # methods from application_controller
     def b(s)
@@ -294,9 +295,9 @@ module Jinda
       Jinda::Service.not_in(:uid=>protected_services).delete_all
     end
     def get_app
-			binding.pry
-      f= MM || "#{Rails.root}/app/jinda/index.mm"
-      dir= File.dirname(f)
+
+			f= MM || "#{Rails.root}/app/jinda/index.mm" || "lib/generators/templates/app/jinda/index.mm"
+			dir= File.dirname(f)
       t= REXML::Document.new(File.read(MM).gsub("\n","")).root
       recheck= true ; first_pass= true
       while recheck
@@ -322,7 +323,219 @@ module Jinda
       end
       return t
     end
-    def controller_exists?(modul)
+
+		########################################################################
+		#                     move code from jinda.rake                        #
+		########################################################################
+		
+		def gen_views
+			t = ["*** generate ui ***"]
+			Jinda::Module.all.each do |m|
+				m.services.each do |s|
+
+					dir ="app/views/#{s.module.code}"
+					unless File.exists?(dir)
+						Dir.mkdir(dir)
+						t << "create directory #{dir}"
+					end
+					
+					if s.code=='link'
+						#f= "app/views/#{s.module.code}/#{s.module.code}.haml"
+						f= "app/views/#{s.module.code}/index.haml"
+						unless File.exists?(f)
+							FileUtils.cp "app/jinda/template/linkview.haml", f
+							t << "create file #{f}"
+						end
+						next   
+					end
+					
+					dir ="app/views/#{s.module.code}/#{s.code}"
+					unless File.exists?(dir)
+						Dir.mkdir(dir)
+						t << "create directory #{dir}"
+					end
+
+					xml= REXML::Document.new(s.xml)
+					xml.elements.each('*/node') do |activity|
+						icon = activity.elements['icon']
+						next unless icon
+						action= freemind2action(icon.attributes['BUILTIN'])
+						next unless ui_action?(action)
+						code_name = activity.attributes["TEXT"].to_s
+						next if code_name.comment?
+						code= name2code(code_name)
+						if action=="pdf"
+							f= "app/views/#{s.module.code}/#{s.code}/#{code}.pdf.prawn"
+						else
+							f= "app/views/#{s.module.code}/#{s.code}/#{code}.html.erb"
+						end
+						unless File.exists?(f)
+							FileUtils.cp "app/jinda/template/view.html.erb", f
+							# ff=File.open(f, 'w'); ff.close
+							t << "create file #{f}"
+						end
+					end
+				end
+			end
+			puts t.join("\n")
+		end
+
+		def process_controllers
+			process_services
+			modules= Jinda::Module.all
+			modules.each do |m|
+				next if controller_exists?(m.code)
+				system("rails generate controller #{m.code}")
+			end
+		end
+
+		def process_models
+
+		# app= get_app
+		# t= ["process models"]
+		#  xml map sample from index.mm 
+		#   node @CREATED=1273819432637 @ID=ID_1098419600 @MODIFIED=1334737006485 @TEXT=Jinda 
+		#    node @CREATED=1273819462973 @ID=ID_282419531 @MODIFIED=1493705904561 @POSITION=right @TEXT=services 
+		#     node @CREATED=1273819465949 @FOLDED=true @ID=ID_855471610 @MODIFIED=1493768913078 @POSITION=right @TEXT=roles 
+		#      node @CREATED=1273819456867 @ID=ID_1677010054 @MODIFIED=1493418874718 @POSITION=left @TEXT=models 
+		#       node @CREATED=1292122118499 @FOLDED=true @ID=ID_1957754752 @MODIFIED=1493705885123 @TEXT=person 
+		#       node @CREATED=1292122236285 @FOLDED=true @ID=ID_959987887 @MODIFIED=1493768919147 @TEXT=address 
+		#       node @CREATED=1493418879485 @ID=ID_1995497233 @MODIFIED=1493718770637 @TEXT=article 
+		#       node @CREATED=1493418915637 @ID=ID_429078131 @MODIFIED=1493418930081 @TEXT=comment 
+		
+			models= @app.elements["//node[@TEXT='models']"] || REXML::Document.new
+			models.each_element('node') do |model|
+				# t << "= "+model.attributes["TEXT"]
+				model_name= model.attributes["TEXT"]
+				next if model_name.comment?
+				model_code= name2code(model_name)
+				model_file= "#{Rails.root}/app/models/#{model_code}.rb"
+		
+				if File.exists?(model_file)
+					doc= File.read(model_file)
+				else
+					system("rails generate model #{model_code}")
+					doc= File.read(model_file)
+				end
+		
+				doc = add_utf8(doc)
+				attr_hash= make_fields(model)
+				doc = add_jinda(doc, attr_hash)
+				# t << "modified:   #{model_file}"
+				File.open(model_file, "w") do |f|
+					f.puts doc
+				end
+
+			end
+
+			# puts t.join("\n")
+		end
+
+		def add_jinda(doc, attr_hash)
+			if doc =~ /#{@btext}/
+				s1,s2,s3= doc.partition(/  #{@btext}.*#{@etext}\n/m)
+				s2= ""
+			else
+				s1,s2,s3= doc.partition("include Mongoid::Document\n")
+			end
+			doc= s1+s2+ <<-EOT
+		#{@btext}
+		include Mongoid::Timestamps
+		EOT
+
+			attr_hash.each do |a|
+				# doc+= "\n*****"+a.to_s+"\n"
+				if a[:edit]
+					doc += "  #{a[:text]}\n"
+				else
+					doc += "  field :#{a[:code]}, :type => #{a[:type].capitalize}\n"
+				end
+			end
+			doc += "  #{@etext}\n"
+			doc + s3
+		end
+
+		def add_utf8(doc)
+			unless doc =~ /encoding\s*:\s*utf-8/
+				doc.insert 0, "# encoding: utf-8\n"
+			else
+				doc
+			end
+		end
+
+		# inspect all nodes that has attached file (2 cases) and replace relative path with absolute path
+		def make_folders_absolute(f,tt)
+			tt.elements.each("//node") do |nn|
+				if nn.attributes['LINK']
+					nn.attributes['LINK']= File.expand_path(File.dirname(f))+"/#{nn.attributes['LINK']}"
+				end
+			end
+		end
+
+		def name2code(s)
+			# rather not ignore # symbol cause it could be comment
+			code, name = s.split(':')
+			code.downcase.strip.gsub(' ','_').gsub(/[^#_\/a-zA-Z0-9]/,'')
+		end
+
+		def model_exists?(model)
+			File.exists? "#{Rails.root}/app/models/#{model}.rb"
+		end
+
+		def make_fields(n)
+			# s= field string used by generate model cli (old style jinda)
+			s= ""
+			# h= hash :code, :type, :edit, :text
+			h= []
+			n.each_element('node') do |nn|
+				text = nn.attributes['TEXT']
+				icon = nn.elements['icon']
+				edit= (icon && icon.attribute('BUILTIN').value=="edit")
+				next if text.comment? && !edit
+
+				# sometimes freemind puts all fields inside a blank node
+				unless text.empty?
+					k,v= text.split(/:\s*/,2)
+					v ||= 'string'
+					v= 'float' if v=~/double/i
+					s << " #{name2code(k.strip)}:#{v.strip} "
+					h << {:code=>name2code(k.strip), :type=>v.strip, :edit=>edit, :text=>text}
+				else
+					nn.each_element('node') do |nnn|
+						icon = nnn.elements['icon']
+						edit1= (icon && icon.attribute('BUILTIN').value=="edit")
+						text1 = nnn.attributes['TEXT']
+						next if text1 =~ /\#.*/
+						k,v= text1.split(/:\s*/,2)
+						v ||= 'string'
+						v= 'float' if v=~/double/i
+						s << " #{name2code(k.strip)}:#{v.strip} "
+						h << {:code=>name2code(k.strip), :type=>v.strip, :edit=>edit1, :text=>text1}
+					end
+				end
+			end
+			# f
+			h
+		end
+
+		# Add method to ruby class String
+		# ###############################
+		class String
+			def comment?
+				self[0]=='#'
+				# self[0]==35 # check if first char is #
+			end
+			def to_code
+				s= self.dup
+				s.downcase.strip.gsub(' ','_').gsub(/[^#_\/a-zA-Z0-9]/,'')
+			end
+		end
+
+		########################################################################
+		#                     END  code from jinda.rake                        #
+		########################################################################
+		
+		def controller_exists?(modul)
       File.exists? "#{Rails.root}/app/controllers/#{modul}_controller.rb"
     end
     def dup_hash(a)
--- spec/jinda_spec.rb
+++ spec/jinda_spec.rb
@@ -1,6 +1,9 @@
 require 'jinda/helpers'
+include Jinda::Helpers
 describe Jinda::Helpers do
-	it 'say something'do
-
-	end
+  it 'index.mm is ready in templates/app/jinda ' do
+    MM = 'lib/generators/jinda/templates/app/jinda/index.mm'
+    result = REXML::Document. new(File.read(MM).gsub('\n', '')).root
+    expect(result).not_to be_nil
+  end
 end
