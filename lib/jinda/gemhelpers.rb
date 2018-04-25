8019f64de7867b1c2759240db876150f1ca030d3
P Kul <1.0@kul.asia>
2018-04-24 23:34:10 -0500

    Updated test using gem.root

--- lib/jinda/gemhelpers.rb
+++ lib/jinda/gemhelpers.rb
@@ -1,10 +1,18 @@
+# This GemHelpers is to make helper able to be test in gem (not in rails)
 module Jinda	
   module GemHelpers
 	  require 'jinda/helpers'
 		include Jinda::Helpers
+			# Find gem root 
+			spec = Gem::Specification.find_by_name("jinda")
+			@gem_root = spec.gem_dir
+
 			def controller_exists?(modul)
-				File.exists? "~/mygem/jinda/lib/generators/jinda/templates/app/controllers/#{modul}_controller.rb"
-				end
+			  spec = Gem::Specification.find_by_name("jinda")
+			  gem_root = spec.gem_dir
+				File.exists? gem_root + "/lib/generators/jinda/templates/app/controllers/#{modul}_controller.rb"
+			end
+
 			#
 			# Mock generate controller for test
 			# Otherwise test will call rails g controller
