8742a7543af322593d912c31c05495c23a157d96
P Kul <1.0@kul.asia>
2018-06-06 07:48:37 -0500

    Update to google login asset

--- lib/generators/jinda/templates/app/models/user.rb
+++ lib/generators/jinda/templates/app/models/user.rb
@@ -8,6 +8,7 @@ class User
   field :role, :type => String
   field :user, :type => String
   field :auth_token, :type => String
+  field :auth_expires_at, :type => DateTime
   field :password_reset_token, :type => String
   field :password_reset_sent_at,:type => DateTime
 
@@ -28,23 +29,27 @@ class User
   end
   
   def self.from_omniauth(auth)
-    where(auth.slice(:uid, :provider, :email)).first_or_create do |user|
-      case auth.provider 
-        when 'identity'
+    # where(auth.slice(:uid, :provider, :email)).first_or_create do |user|
+		where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+			case auth.provider 
+      when 'identity'
           identity = Identity.find auth.uid
           user.code = identity.code
           user.email = identity.email
-        else
+      else
           user.email = auth.info.email
           user.uid = auth.uid
           user.provider = auth.provider
           user.code = auth.info.name
           user.role = "M"
-        end
-      end
-    end
-         
-  def ma_secured?
+					user.auth_token = auth.credentials.token
+					user.auth_expires_at = Time.at(auth.credentials.expires_at)
+					user.save!
+			end
+		end
+	end
+ 
+	def ma_secured?
     role.upcase.split(',').include?(ma_secured_ROLE)
   end
   
@@ -53,7 +58,7 @@ class User
     self.password_reset_sent_at = Time.zone.now
     save!
     UserMailer.password_reset(self).deliver
-end
+	end
 
 
 end
