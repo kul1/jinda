7128a2c415bcb861840414b1197855824e86774e
kul1 <1.0@kul.asia>
2019-08-14 10:55:48 -0400

    updated template for view and model

--- lib/generators/jinda/templates/app/models/jinda/doc.rb
+++ lib/generators/jinda/templates/app/models/jinda/doc.rb
@@ -1,31 +1,15 @@
-# -*- encoding : utf-8 -*-
+# encoding: utf-8
 class Jinda::Doc
   include Mongoid::Document
+  # jinda begin
   include Mongoid::Timestamps
-  field :name, :type => String
-  field :filename, :type => String
-  field :content_type, :type => String
-  field :data_text, :type => String
-  field :url, :type => String
-  field :basename, :type => String
-  field :cloudinary, :type => Boolean
-  belongs_to :xmain, :class_name => "Jinda::Xmain"
-  belongs_to :runseq, :class_name => "Jinda::Runseq"
-  belongs_to :user, :class_name => "Jinda::User"
-  belongs_to :service, :class_name => "Jinda::Service"
-  field :ip, :type => String
-  field :ma_display, :type => Boolean
-  field :ma_secured, :type => Boolean
-
-  def self.search(q, page, per_page=PER_PAGE)
-    paginate :per_page=>per_page, :page => page, :conditions =>
-      ["content_type=? AND data_text LIKE ? AND (ma_secured=? OR ma_user_id=?)",
-      "output", "%#{q}%", false, session[:user_id] ],
-      :order=>'ma_xmain_id DESC', :select=>'DISTINCT ma_xmain_id'
-  end
-  def self.search_ma_secured(q, page, per_page=PER_PAGE)
-    paginate :per_page=>per_page, :page => page, :conditions =>
-      ["content_type=? AND data_text LIKE ?", "output", "%#{q}%" ],
-      :order=>'ma_xmain_id DESC', :select=>'DISTINCT ma_xmain_id'
-  end
+  field :issue_on, :type => Date
+  field :process_at, :type => Date
+  field :summary, :type => String
+  field :dscan, :type => String
+  field :hotel, :type => String
+  field :done, :type => Boolean
+  field :rnum, :type => String
+  field :confidential, :type => String
+  # jinda end
 end
