1a163aab3423e671ed9def1adbfa64977dfc8e83
Prateep Kul <1.0@kul.asia>
2020-11-26 13:52:06 -0600

    Add .rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
 require 'active_support/core_ext/regexp'
-require 'mongoid'
+require 'rails/mongoid'
+Mongoid.load!('./mongoid.yml')
 require_relative '../lib/generators/jinda/templates/app/models/jinda/module.rb'
 require_relative '../lib/generators/jinda/templates/app/models/jinda/service.rb'
 #Dir[File.dirname(__FILE__) + './lib/generators/jinda/templates/app/models/jinda'].each {|file| puts file}
 require 'dotenv'
 Dotenv.load
-Mongoid.load!('./mongoid.yml')
