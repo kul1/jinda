e4983b106ed36395efc01b3867c42e472afeb979
Prateep Kul <1.0@kul.asia>
2021-09-06 08:25:43 -0500

    Removed spec test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,10 @@
 require 'pry'
+require 'rails'
 require 'active_support/core_ext/regexp'
 # require 'rails/mongoid'
 # Mongoid.load!('./mongoid.yml')
-binding.pry
-require_relative '../lib/generators/jinda/templates/app/models/jinda/module.rb'
-require_relative '../lib/generators/jinda/templates/app/models/jinda/service.rb'
+# require_relative '../lib/generators/jinda/templates/app/models/jinda/module.rb'
+# require_relative '../lib/generators/jinda/templates/app/models/jinda/service.rb'
 #Dir[File.dirname(__FILE__) + './lib/generators/jinda/templates/app/models/jinda'].each {|file| puts file}
 require 'dotenv'
 Dotenv.load
