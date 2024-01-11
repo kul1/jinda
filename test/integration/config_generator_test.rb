eabb22fbefe348554ee3cacfad9c8e911067656f
Prateep Kul <1.0@kul.asia>
2024-01-11 16:56:07 -0600

    c241116 rm shop with jinda & dummy test

---
+++ test/integration/config_generator_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+require 'generators/jinda/config/config_generator'
+require 'pry'
+
+class ConfigGeneratorTest < Rails::Generators::TestCase
+  tests Jinda::Generators::ConfigGenerator
+  destination File.expand_path('../../tmp', __FILE__)
+  setup :prepare_destination
+
+  test "backup_files method moves .env file to .env-bak if it exists" do
+    FileUtils.touch("#{destination_root}/.env")
+    assert File.exist?("#{destination_root}/.env")
+
+    run_generator
+    refute File.exist?("#{destination_root}/.env")
+    binding.pry
+    assert File.exist?("#{destination_root}/.env-bak")
+  end
+
+  test "gen_image_store method creates an 'upload' directory" do
+    run_generator
+    assert File.directory?("#{destination_root}/upload")
+  end
+
+  # Add more tests for other methods as needed...
+
+end
+
