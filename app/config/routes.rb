fdd328e81bbc4967b77a9350ca3e2e4f1c1f3cc9
Prateep Kul <1.0@kul.asia>
2024-03-31 08:19:08 -0500

    jinda aws fix

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -42,3 +42,5 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+# Define Routes constant
+Routes = Rails.application.routes
