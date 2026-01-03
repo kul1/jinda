f4ddb614728cf732a6f08a1f82a421aa46db7b7e
Prateep Kul <1.0@kul.asia>
2026-01-02 22:49:11 -0600

    rm version: '3.2'

--- lib/jinda/railtie.rb
+++ lib/jinda/railtie.rb
@@ -9,6 +9,12 @@ module Jinda
     initializer 'testing' do |_app|
       ActiveSupport.on_load(:action_controller) { include Jinda::Helpers }
     end
+    
+    initializer 'jinda.omniauth', after: :load_config_initializers do |_app|
+      # Ensure OmniAuth allows both GET and POST for callback routes
+      OmniAuth.config.allowed_request_methods = [:post, :get] if defined?(OmniAuth)
+    end
+    
     rake_tasks do
       load 'tasks/jinda.rake'
     end
